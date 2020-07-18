4e68182ac4bcf2d28fa6315a440a6663c928019e
Aida Delos Reyes <acsdelosreyes@gmail.com>
2020-07-19 00:22:44 +0800

    Initial commit

---
+++ budget_app_clone/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from budget_app_clone.users.models import User
+from budget_app_clone.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
